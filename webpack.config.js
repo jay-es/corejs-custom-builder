fa04b8ffe2c790092cbb2a84dd03c3e629b8e115
Jun Shindo <jshindo@gladd.jp>
2018-04-23 23:25:28 +0900

    webpack3 -> 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,10 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
+  mode: 'production',
   entry: './entry.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'polyfill.min.js'
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-  ],
+  }
 }
