adfccda0557efbba6b75eee70323ebe176adf769
Jun Shindo <jshindo@gladd.jp>
2018-04-24 23:29:34 +0900

    テスト結果をHTMLで出力

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
-function test(bool) {
-  var targets = {
-    'core-js/fn/array/find':        Array.prototype.find,
-    'core-js/fn/array/find-index':  Array.prototype.findIndex,
-    'core-js/fn/array/includes':    Array.prototype.includes,
+function getTargets() {
+  return {
+    'core-js/fn/array/find':          Array.prototype.find,
+    'core-js/fn/array/find-index':    Array.prototype.findIndex,
+    'core-js/fn/array/includes':      Array.prototype.includes,
 
-    'core-js/fn/object/assign':   Object.assign,
-    'core-js/fn/object/entries':  Object.entries,
-    'core-js/fn/object/values':   Object.values,
+    'core-js/fn/object/assign':       Object.assign,
+    'core-js/fn/object/entries':      Object.entries,
+    'core-js/fn/object/values':       Object.values,
 
     'core-js/fn/string/includes':     String.prototype.includes,
     'core-js/fn/string/starts-with':  String.prototype.startsWith,
@@ -14,12 +14,21 @@ function test(bool) {
     'core-js/fn/string/pad-start':    String.prototype.padStart,
     'core-js/fn/string/pad-end':      String.prototype.padEnd,
   };
-
-  Object.keys(targets).forEach(function(key) {
-    var result = targets[key] === undefined;
-    console.assert(result === bool, key);
-  });
 }
 
-test(true);
-setTimeout(test.bind(null, false), 10);
+var before = getTargets();
+
+setTimeout(function () {
+  new Vue({
+    el: '#app',
+    data: {
+      after: getTargets(),
+      before: before,
+    },
+    methods: {
+      cellClass: function (val) {
+        return val ? 'ok' : 'ng';
+      }
+    }
+  });
+}, 20);
