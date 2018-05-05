20c6f67935e578c296caf70680c2846a2e26d0ff
Jun Shindo <jshindo@gladd.jp>
2018-05-05 23:19:59 +0900

    モジュールのリストを拡充

--- modules.js
+++ modules.js
@@ -1,15 +1,80 @@
 module.exports = Object.entries({
-  'core-js/fn/array/find':          'Array.prototype.find',
-  'core-js/fn/array/find-index':    'Array.prototype.findIndex',
-  'core-js/fn/array/includes':      'Array.prototype.includes',
+  // ECMAScript 6: Object
+  'core-js/fn/object/assign':           'Object.assign',
+  'core-js/fn/object/is':               'Object.is',
 
-  'core-js/fn/object/assign':       'Object.assign',
-  'core-js/fn/object/entries':      'Object.entries',
-  'core-js/fn/object/values':       'Object.values',
+  // ECMAScript 6: Array
+  'core-js/fn/array/from':              'Array.from',
+  'core-js/fn/array/of':                'Array.of',
+  'core-js/fn/array/copy-within':       'Array.prototype.copyWithin',
+  'core-js/fn/array/fill':              'Array.prototype.fill',
+  'core-js/fn/array/find':              'Array.prototype.find',
+  'core-js/fn/array/find-index':        'Array.prototype.findIndex',
+  'core-js/fn/array/keys':              'Array.prototype.keys',
+  'core-js/fn/array/values':            'Array.prototype.values',
+  'core-js/fn/array/entries':           'Array.prototype.entries',
 
-  'core-js/fn/string/includes':     'String.prototype.includes',
-  'core-js/fn/string/starts-with':  'String.prototype.startsWith',
-  'core-js/fn/string/ends-with':    'String.prototype.endsWith',
-  'core-js/fn/string/pad-start':    'String.prototype.padStart',
-  'core-js/fn/string/pad-end':      'String.prototype.padEnd',
+  // ECMAScript 6: String
+  'core-js/fn/string/from-code-point':  'String.fromCodePoint',
+  'core-js/fn/string/raw':              'String.raw',
+  'core-js/fn/string/includes':         'String.prototype.includes',
+  'core-js/fn/string/starts-with':      'String.prototype.startsWith',
+  'core-js/fn/string/repeat':           'String.prototype.repeat',
+  'core-js/fn/string/ends-with':        'String.prototype.endsWith',
+  'core-js/fn/string/code-point-at':    'String.prototype.codePointAt',
+
+  // ECMAScript 6: Number
+  'core-js/fn/number/is-finite':        'Number.isFinite',
+  'core-js/fn/number/is-nan':           'Number.isNaN',
+  'core-js/fn/number/is-integer':       'Number.isInteger',
+  'core-js/fn/number/parse-float':      'Number.parseFloat',
+  'core-js/fn/number/parse-int':        'Number.parseInt',
+  'core-js/fn/number/epsilon':          'Number.EPSILON',
+  'core-js/fn/number/max-safe-integer': 'Number.MAX_SAFE_INTEGER',
+  'core-js/fn/number/min-safe-integer': 'Number.MIN_SAFE_INTEGER',
+
+  // ECMAScript 6: Math
+  'core-js/fn/math/acosh':              'Math.acosh',
+  'core-js/fn/math/asinh':              'Math.asinh',
+  'core-js/fn/math/atanh':              'Math.atanh',
+  'core-js/fn/math/cbrt':               'Math.cbrt',
+  'core-js/fn/math/clz32':              'Math.clz32',
+  'core-js/fn/math/cosh':               'Math.cosh',
+  'core-js/fn/math/expm1':              'Math.expm1',
+  'core-js/fn/math/fround':             'Math.fround',
+  'core-js/fn/math/hypot':              'Math.hypot',
+  'core-js/fn/math/imul':               'Math.imul',
+  'core-js/fn/math/log1p':              'Math.log1p',
+  'core-js/fn/math/log10':              'Math.log10',
+  'core-js/fn/math/log2':               'Math.log2',
+  'core-js/fn/math/sign':               'Math.sign',
+  'core-js/fn/math/sinh':               'Math.sinh',
+  'core-js/fn/math/tanh':               'Math.tanh',
+  'core-js/fn/math/trunc':              'Math.trunc',
+
+  // ECMAScript 6: Promise
+  'core-js/fn/promise':                 'Promise',
+
+  // ECMAScript 6: Symbol
+  'core-js/es6/symbol':                 'Symbol',
+
+  // ECMAScript 6: Collections
+  'core-js/fn/map':                     'Map',
+  'core-js/fn/set':                     'Set',
+  'core-js/fn/weak-map':                'WeakMap',
+  'core-js/fn/weak-set':                'WeakSet',
+
+  // ECMAScript 6: Typed Arrays
+  'core-js/es6/typed':                  'ArrayBuffer',
+
+  // ECMAScript 6: Reflect
+  'core-js/es6/reflect':                'Reflect',
+
+  // Stage 4 proposals
+  'core-js/fn/array/includes':          'Array.prototype.includes',
+  'core-js/fn/object/values':           'Object.values',
+  'core-js/fn/object/entries':          'Object.entries',
+  'core-js/fn/object/get-own-property-descriptors': 'Object.getOwnPropertyDescriptors',
+  'core-js/fn/string/pad-start':        'String.prototype.padStart',
+  'core-js/fn/string/pad-end':          'String.prototype.padEnd',
 });
