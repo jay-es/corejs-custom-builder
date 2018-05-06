b60aded9bfd970e7c7da56883bf69079fa5ffbb9
Jun Shindo <jshindo@gladd.jp>
2018-05-06 19:47:42 +0900

    modulesのkeyとvalueを反転

--- prebuild.js
+++ prebuild.js
@@ -7,11 +7,11 @@ fs.existsSync(DIST_DIR) || fs.mkdirSync(DIST_DIR);
 
 // entry.js
 const entryFile = path.resolve(DIST_DIR, 'entry.js');
-const entryContent = modules.map(([key]) => `require('${key}');`).join('\n');
+const entryContent = Object.values(modules).map(val => `require('core-js/modules/${val}');`).join('\n');
 fs.writeFileSync(entryFile, entryContent);
 
 // get-methods.js
 const testFile = path.resolve(DIST_DIR, 'get-methods.js');
-const lines = modules.map(([key, val]) => `  '${key}': ${val},`).join('\n');
+const lines = Object.keys(modules).map(key => `  '${key}': ${key},`).join('\n');
 const testContent = `function getMethods() {\n return {\n${lines}\n };\n}`;
 fs.writeFileSync(testFile, testContent);
